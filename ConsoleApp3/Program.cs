cf3893ddd9e54ad8b208cee925c0359945a76544
saksham rana <0blackbuck@gmail.com>
2019-01-22 18:36:13 -0500

    program 4

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,11 +2,27 @@
 
 namespace ConsoleApp3
 {
-    class Program
+    class Program //program 4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i, val, sum = 0;
+            int t = 1;
+
+            Console.Write("\n\n");
+            Console.Write("Calculate the sum of the series :\n");
+            Console.Write("\n\n");
+            Console.Write("Input from user:");
+            val = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= val; i++)
+            {
+                Console.Write("{0} + ", t);
+                sum = sum + t;
+                t = (t) + 1;
+            }
+            Console.Write("\nThe Sum is : {0}\n", sum);
+            Console.ReadLine();
         }
     }
 }
